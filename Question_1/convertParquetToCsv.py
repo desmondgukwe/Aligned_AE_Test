a6e4def1f1c031bf88838ee849c8079a60a77160
Desmond Gukwe <44749674+desmondgukwe@users.noreply.github.com>
2025-05-18 01:46:43 +0200

    Update convertParquetToCsv.py

--- Question_1/convertParquetToCsv.py
+++ Question_1/convertParquetToCsv.py
@@ -1,28 +1,30 @@
 import boto3
 import pandas as pd
 import io
-
+#The scripts attempts to Get file location from trigger event then  Download and convert the file & lastly Save as CSV to new location
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
-
-    obj = s3.get_object(Bucket=bucket, Key=key)
-    df = pd.read_parquet(io.BytesIO(obj['Body'].read()))
-
-    out_buffer = io.StringIO()
-    df.to_csv(out_buffer, index=False)
-
-    s3.put_object(
-        Bucket='processed-bucket-analytics',
-        Key=key.replace('.parquet', '.csv'),
-        Body=out_buffer.getvalue()
-    )
-
-    print(f"Process completed: {key} converted and saved to processed-bucket-analytics")
-    return {'statusCode': 200, 'body': 'converted'}
-
-
-
+    
+    try:
+        
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        file_key = event['Records'][0]['s3']['object']['key']
+        
+        
+        parquet_data = s3.get_object(Bucket=bucket, Key=file_key)['Body'].read()
+        df = pd.read_parquet(io.BytesIO(parquet_data))
+        
+        
+        csv_file = io.StringIO()
+        df.to_csv(csv_file, index=False)
+        s3.put_object(
+            Bucket='processed-bucket-analytics',
+            Key=file_key.replace('.parquet', '.csv'),
+            Body=csv_file.getvalue()
+        )
+        
+        return {'status': 'success'}
+        
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
 
